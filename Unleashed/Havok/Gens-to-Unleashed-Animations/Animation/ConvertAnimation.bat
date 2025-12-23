351add2afc896e232619d1588231b19921073157
EM <ale69294@gmail.com>
2025-12-23 23:34:06 +0300

    Fix batch script

--- Unleashed/Havok/Gens-to-Unleashed-Animations/Animation/ConvertAnimation.bat
+++ Unleashed/Havok/Gens-to-Unleashed-Animations/Animation/ConvertAnimation.bat
@@ -1,3 +1,3 @@
-for %%f in (source\*.hkx) do AssetCcGens.exe -x "%%f" "output\%%~nf.xml"
-for %%f in (output\*.xml) do (python ..\Havok.py "%%f" "output\%%~nf_550.xml" & if exist "output\%%~nf_550.xml" (del "%%f" & ren "output\%%~nf_550.xml" "%%~nf.xml"))
-for %%f in (output\*.xml) do (AssetCcUnleashed.exe --rules4001 "%%f" "output\%%~nf.hkx" 2>nul & del "%%f")
+for %%f in (source\*.hkx) do AssetCcGens.exe -x "%%f" "output\%%~nxf.xml"
+for %%f in (output\*.xml) do gens2unleashed-animconverter.exe "%%f" "%%f"
+for %%f in (output\*.xml) do (AssetCcUnleashed.exe --rules4001 "%%f" "%%~dpnf" 2>nul & del "%%f")
